fe4318c0b5f605639feb0a8f4124cccbb8881f70
Morgan Koh <delacrixmorgan@pm.me>
2026-04-27 21:02:59 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/birthdays.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
